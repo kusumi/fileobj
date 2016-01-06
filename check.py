d117cb62ff69421ca702f1f1a0a4d5c160766089
Wouter van Kesteren <woutershep@gmail.com>
2016-01-06 19:25:50 +0100

    Make 'build' and 'install --root' work.
    This moves the checking to a separate file so that we can ensure that it
    will not be called before it is succesfully installed into the system.

---
+++ check.py
@@ -0,0 +1,24 @@
+if __name__ == '__main__':
+    import os
+    import sys
+
+    import src.nodep
+    src.nodep.test()
+
+    import fileobj.package
+    import fileobj.version
+    l = []
+    for d in fileobj.package.get_paths():
+        for x in fileobj.package.iter_module_name():
+            s = x[len(fileobj.package.get_prefix()):]
+            s = s.replace(".", "/") + ".py"
+            a = os.path.join(d, s)
+            b = os.path.join("./src", s)
+            if os.path.isfile(a) and not os.path.isfile(b):
+                l.append(a)
+    if l:
+        for f in l:
+            sys.stderr.write("### %s has no such file %s\n" %
+                (fileobj.version.get_version_string(), f))
+        sys.stderr.write("### Prefer uninstalling older version if exists\n")
+        sys.exit(2)
--- setup.py
+++ setup.py
@@ -26,21 +26,3 @@ if __name__ == '__main__':
         scripts     = ["script/fileobj"],
         packages    = ["fileobj", "fileobj.ext"],
         package_dir = {"fileobj" : "src", "fileobj.ext" : "src/ext",})
-
-    import fileobj.package
-    import fileobj.version
-    l = []
-    for d in fileobj.package.get_paths():
-        for x in fileobj.package.iter_module_name():
-            s = x[len(fileobj.package.get_prefix()):]
-            s = s.replace(".", "/") + ".py"
-            a = os.path.join(d, s)
-            b = os.path.join("./src", s)
-            if os.path.isfile(a) and not os.path.isfile(b):
-                l.append(a)
-    if l:
-        for f in l:
-            sys.stderr.write("### %s has no such file %s\n" %
-                (fileobj.version.get_version_string(), f))
-        sys.stderr.write("### Prefer uninstalling older version if exists\n")
-        sys.exit(2)
